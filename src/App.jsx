fb397bbdddcdd5fd9b5eb8797000c8d4e25eb591
Daniel Landa <landadlh603@gmail.com>
2025-03-21 20:33:28 +0100

    project terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,110 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
-  const [input , setInput] = useState("");
+  const [input, setInput] = useState("");
 
   const getToDos = async () => {
-    const response = await fetch('https://playground.4geeks.com/todo/users/dan');
-    console.log(response)
-    if(!response.ok){
+    const response = await fetch(
+      "https://playground.4geeks.com/todo/users/dan"
+    );
+    console.log(response);
+    if (!response.ok) {
       createDanUser();
       return;
     }
-    const data = await response.json()
-    console.log(data)
-    setTasks(data.todos)
-  }
+    const data = await response.json();
+    setTasks(data.todos);
+  };
 
   const createDanUser = async () => {
-    const response = await fetch('https://playground.4geeks.com/todo/users/dan', {
-      method: 'POST'
-    });
-    const data = await response.json()
-    console.log(data)
-  }
-
-  const addTask = async () => {
-    if(input.trim()){
-      const response = await fetch('https://playground.4geeks.com/todo/todos/dan',{
-        method: 'POST',
-        body: JSON.stringify({
-          'label': input,
-          'is_done': false
-        }),
+    const response = await fetch(
+      "https://playground.4geeks.com/todo/users/dan",
+      {
+        method: "POST",
+      }
+    );
+    const data = await response.json();
+  };
+  const deleteTask = async (id) => {
+    await fetch(
+      "https://playground.4geeks.com/todo/todos/" + id,
+      {
+        method: "DELETE",
         headers: {
-          'Content-Type': 'application/json'
+          'Contact-Type': 'application/json; charset=UTF-8'
         }
-      })
-      const data = await response.json()
-      if(data){
+      }
+    );
+    setTasks(tasks.filter(task => task.id !== id))
+  };
+  const addTask = async () => {
+    if (input.trim()) {
+      const response = await fetch(
+        "https://playground.4geeks.com/todo/todos/dan",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            label: input,
+            is_done: false,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      if (data) {
         getToDos();
         setInput("");
       }
     }
-  }
-
-  const deleteTask = (id) => {
-    setTasks(tasks.filter(task => task.id !== id))
-  }
-
+  };
+  async function doneTask() {}
   const handleKeyDown = (e) => {
-    if(e.key === 'Enter'){
-      addTask()
+    if (e.key === "Enter") {
+      addTask();
     }
-  }
+  };
 
   useEffect(() => {
-    getToDos()
-  },[])
+    getToDos();
+  }, []);
 
   return (
     <>
-      <h1>To-Do-List</h1>
-      <input 
-      type="text" 
-      className="btn-Add"
-      value={input} 
-      onKeyDown={handleKeyDown}
-      onChange={(e) => setInput(e.target.value)}
-      placeholder="Nueva Tarea ..."
-      />
-      <button className="btn-Add" onClick={addTask}>➕</button>
-      <ul>
-        {tasks.map(task => (
-          <li key={task.id} >
-            <span>
-              {task.label}{task.id}
-            </span>
-            <button className="btn-Delete" onClick={()=> deleteTask(task.id)}>❌</button>
-          </li>
-        ))}
-      </ul>
+      <div className="CardLista3">
+        <div className="CardLista2">
+          <div className="CardLista">
+            <h1>To-Do-List</h1>
+            <input
+              type="text"
+              className="btn-Add"
+              value={input}
+              onKeyDown={handleKeyDown}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Nueva Tarea ..."
+            />
+            <button className="btn-Add" onClick={addTask}>
+              ➕
+            </button>
+            <ul>
+              {tasks.map((task) => (
+                <li key={task.id}>
+                  <input type="checkbox" id={task.id} onClick={doneTask} />
+                  <span>{task.label}</span>
+                  <button className="btn-Delete" onClick={() => deleteTask(task.id)}>
+                    ❌
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
 
-export default App; 
+export default App;
