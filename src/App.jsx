96f4b055b434ab051256db60633e7f05909db345
Daniel Landa <landadlh603@gmail.com>
2025-03-21 18:25:29 +0100

    insertanto y leyendo las tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
+import { useState , useEffect } from "react";
 import "./App.css";
-import { useState } from "react";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
   const [input , setInput] = useState("");
 
-  const addTask = () => {
+  const getToDos = async () => {
+    const response = await fetch('https://playground.4geeks.com/todo/users/dan');
+    console.log(response)
+    if(!response.ok){
+      createDanUser();
+      return;
+    }
+    const data = await response.json()
+    console.log(data)
+    setTasks(data.todos)
+  }
+
+  const createDanUser = async () => {
+    const response = await fetch('https://playground.4geeks.com/todo/users/dan', {
+      method: 'POST'
+    });
+    const data = await response.json()
+    console.log(data)
+  }
+
+  const addTask = async () => {
     if(input.trim()){
-      setTasks([...tasks, {
-        id: Math.random(),
-        text: input,
-        done: false
-      }])
-      setInput("")
+      const response = await fetch('https://playground.4geeks.com/todo/todos/dan',{
+        method: 'POST',
+        body: JSON.stringify({
+          'label': input,
+          'is_done': false
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const data = await response.json()
+      if(data){
+        getToDos();
+        setInput("");
+      }
     }
   }
 
@@ -20,6 +49,15 @@ const App = () => {
     setTasks(tasks.filter(task => task.id !== id))
   }
 
+  const handleKeyDown = (e) => {
+    if(e.key === 'Enter'){
+      addTask()
+    }
+  }
+
+  useEffect(() => {
+    getToDos()
+  },[])
 
   return (
     <>
@@ -28,6 +66,7 @@ const App = () => {
       type="text" 
       className="btn-Add"
       value={input} 
+      onKeyDown={handleKeyDown}
       onChange={(e) => setInput(e.target.value)}
       placeholder="Nueva Tarea ..."
       />
@@ -36,7 +75,7 @@ const App = () => {
         {tasks.map(task => (
           <li key={task.id} >
             <span>
-              {task.text}
+              {task.label}{task.id}
             </span>
             <button className="btn-Delete" onClick={()=> deleteTask(task.id)}>❌</button>
           </li>
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Appcopy from '../appcopy.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Appcopy/>
+    <App/>
   </StrictMode>,
 )
